7a62696f984e62bba6d1a2704ea172f47ba59120
mahdi <mahdihidayah54@gmail.com>
2024-08-15 05:55:38 +0700

    update commit

--- async/4.ajax.js
+++ async/4.ajax.js
@@ -1,1 +1,13 @@
 const xhr = new XMLHttpRequest () //ini ajax
+
+xhr.open ("GET", "https://api.npoint.io/9a2f5cb05b4807023ce7", true)
+
+xhr.onerror = () => {
+    console.log ("Network error!")
+}
+
+xhr.onload = () => {
+    console.log (xhr.responseText)
+}
+
+xhr.send()
